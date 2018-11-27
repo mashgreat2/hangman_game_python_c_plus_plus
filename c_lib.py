3750eedacf6e347bbb8e1edd6eff7f041fe16de2
Vy Quynh Nguyen <vqn2@cornell.edu>
2018-11-26 22:20:12 -0500

    added compatible function for check_letter_in_word

--- c_lib.py
+++ c_lib.py
@@ -5,6 +5,8 @@ c_lib = cdll.LoadLibrary('./_c_lib.so')
 # calling the c++ functions.
 c_lib.greet_player_c.argtype = ctypes.c_char_p
 c_lib.greet_player_c.restype = ctypes.c_char_p
+# c_lib.check_letter_in_word.argtype = ctypes.c_char_p
+# c_lib.check_letter_in_word.restype = ctypes.c_int
 c_lib.update_guessed_correctly.argtype = ctypes.c_int
 c_lib.update_guessed_correctly.restype = ctypes.c_int
 c_lib.generate_word.restype = ctypes.c_char_p
@@ -24,6 +26,9 @@ def ctypes_greet_player(name):
     _msg = c_lib.greet_player_c(name.encode('utf-8'))
 
     return _msg.decode('utf-8')
+
+# def ctypes_check_letter_in_word(str, letter):
+    # return c_lib.check_letter_in_word(str, letter)
 
 def ctypes_update_guessed_correctly(n, b):
     return c_lib.update_guessed_correctly(n, b)
